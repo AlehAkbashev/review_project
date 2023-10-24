c11eb257d33f0b9b100e1bcb4c9590a4b2e59f37
Георгий Петухов <gosha_pet@mail.ru>
2023-10-24 18:59:46 +0300

    Added verbose name

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -2,29 +2,35 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.CharField(max_length=50)
+    name = models.CharField(max_length=256, verbose_name='category_name')
+    slug = models.CharField(max_length=50, verbose_name='category_slug')
 
     def __str__(self):
         return self.name
 
 
 class Genres(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.CharField(max_length=50)
+    name = models.CharField(max_length=256, verbose_name='genres_name')
+    slug = models.CharField(max_length=50, verbose_name='genres_slug')
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=256)
-    year = models.IntegerField()
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=256, verbose_name='title_name')
+    year = models.IntegerField(verbose_name='title_year')
+    description = models.TextField(blank=True, verbose_name='title_description')
     category = models.ForeignKey(
-        Categories, on_delete=models.CASCADE, related_name='Categories')
+        Categories,
+        on_delete=models.CASCADE,
+        verbose_name='title_categories',
+        related_name='categories')
     genre = models.ManyToManyField(
-        Genres, on_delete=models.CASCADE, related_name='Genres')
+        Genres,
+        on_delete=models.CASCADE,
+        verbose_name='title_genres',
+        related_name='genres')
 
     def __str__(self):
         return self.name
