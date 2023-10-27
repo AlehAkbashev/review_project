dc819c7ee29b39c3d6d49b2f5ee6e09785e112fa
isko118 <vohedoque@yandex.ru>
2023-10-27 15:23:13 +0300

    Removed unnecessary serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,13 +4,6 @@ import datetime as dt
 from reviews.models import Categories, Genres, Title, Comment, Review
 
 
-class CategoriesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Categories
-        fields = '__all__'
-
-
-
 class GenresSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
@@ -33,17 +26,6 @@ class TitleSerializer(serializers.ModelSerializer):
         if year > year+1:
             raise serializers.ValidationError("Check year of title")
         return value
-        model = Genres
-        fields = '__all__'
-
-
-class TitleSerializer(serializers.ModelSerializer):
-    category = CategoriesSerializer()
-    genre = GenresSerializer(many=True)
-
-    class Meta:
-        model = Title
-        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -58,4 +40,3 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
-
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import GenresSerializer, TitleSerializer, CategoriesSerializer
+
 from reviews.models import Genres, Title, Categories
 
 
