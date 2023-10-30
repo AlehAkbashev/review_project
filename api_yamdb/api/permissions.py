4cf1d36a7c2d877349c30725ad449ed1b2b158c8
Георгий Петухов <gosha_pet@mail.ru>
2023-10-30 16:28:09 +0300

    Some New Updates

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,5 +1,19 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
+class ReaderOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            if (request.user.is_authenticated
+            and request.user.role == 'admin'):
+                return True
+            return False
+        
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == 'admin'
+    
 
 class AdminAccess(BasePermission):
     def has_permission(self, request, view):
