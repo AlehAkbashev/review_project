ac111c868db011516277a86e30bce1c37f4e9858
isko118 <vohedoque@yandex.ru>
2023-10-30 22:33:14 +0300

    Changed seriazliers

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -3,16 +3,13 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.tokens import default_token_generator
 
-
 User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-
     class Meta:
         fields = ('email', 'username')
         model = User
-    
 
 
 class MyTokenObtainPairSerializer(serializers.ModelSerializer):
@@ -25,6 +22,6 @@ class MyTokenObtainPairSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         user = get_object_or_404(User, username=data['username'])
-        if not default_token_generator.check_token(user, data['confirmation_code']):
+        if default_token_generator.check_token(user, data['confirmation_code']):
             raise serializers.ValidationError('Confirmation code does not match')
         return data
